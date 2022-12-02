990bc16e60ccd6d947083201a6f99466b06cc5a8
Alexandr <jwchaganav@gmail.com>
2022-12-02 16:25:16 +0400

    Создал обход массива для определения будущиего массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!".Le);
+﻿string[] arrayString = {"Вася","Кира","моль","2","ха","0","ухо","горло","нос","вилка","бутерброт"};
+int sizeWord = 3;
+
+int count = SizeArray(arrayString, sizeWord);
+string[] arrayWold = new string[count];
+
+
+// Функции
+int SizeArray(string[] array,int max)
+{
+    int count = 0;
+    for(int i=0; i <= array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+            count++;
+    }
+    return count;
+}
+
+
